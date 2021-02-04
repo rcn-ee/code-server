ae2d4551131f89f3e8837d4473087fa55bc241a2
Joe Previte <jjprevite@gmail.com>
2021-02-04 14:53:54 -0700

    feat: add cookie utils for e2e tests

--- test/util.test.ts
+++ test/util.test.ts
@@ -1,4 +1,7 @@
-import { normalize } from "../src/common/util"
+import { Cookie } from "playwright"
+import { checkForCookie, createCookieIfDoesntExist, normalize } from "../src/common/util"
+import { Cookie as CookieEnum } from "../src/node/routes/login"
+import { hash } from "../src/node/util"
 
 describe("util", () => {
   describe("normalize", () => {
@@ -13,6 +16,62 @@ describe("util", () => {
     it("should preserve trailing slash if it exists", () => {
       expect(normalize("qux///", true)).toBe("qux/")
       expect(normalize("qux", true)).toBe("qux")
+    })
+  })
+
+  describe("checkForCookie", () => {
+    it("should check if the cookie exists and has a value", () => {
+      const PASSWORD = "123supersecure!"
+      const fakeCookies: Cookie[] = [
+        {
+          name: CookieEnum.Key,
+          value: hash(PASSWORD),
+          domain: "localhost",
+          secure: false,
+          sameSite: "Lax",
+          httpOnly: false,
+          expires: 18000,
+          path: "/",
+        },
+      ]
+      expect(checkForCookie(fakeCookies, CookieEnum.Key)).toBe(true)
+    })
+    it("should return false if there are no cookies", () => {
+      const fakeCookies: Cookie[] = []
+      expect(checkForCookie(fakeCookies, "key")).toBe(false)
+    })
+  })
+
+  describe("createCookieIfDoesntExist", () => {
+    it("should create a cookie if it doesn't exist", () => {
+      const PASSWORD = "123supersecure"
+      const cookies: Cookie[] = []
+      const cookieToStore = {
+        name: CookieEnum.Key,
+        value: hash(PASSWORD),
+        domain: "localhost",
+        secure: false,
+        sameSite: "Lax" as const,
+        httpOnly: false,
+        expires: 18000,
+        path: "/",
+      }
+      expect(createCookieIfDoesntExist(cookies, cookieToStore)).toStrictEqual([cookieToStore])
+    })
+    it("should return the same cookies if the cookie already exists", () => {
+      const PASSWORD = "123supersecure"
+      const cookieToStore = {
+        name: CookieEnum.Key,
+        value: hash(PASSWORD),
+        domain: "localhost",
+        secure: false,
+        sameSite: "Lax" as const,
+        httpOnly: false,
+        expires: 18000,
+        path: "/",
+      }
+      const cookies: Cookie[] = [cookieToStore]
+      expect(createCookieIfDoesntExist(cookies, cookieToStore)).toStrictEqual(cookies)
     })
   })
 })
